f270e909863d55dc40ccce4b7f8dd98c8ec93bb6
Jo <loluvulol@gmail.com>
2022-08-29 15:53:42 +1000

    Set up app

---
+++ app.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongo = require("mongodb");
+const mongoClient = mongo.MongoClient;
+let ObjectId = mongo.ObjectId;
+const url = "mongodb://127.0.0.1:27017";
+
+app.use("/", express.static(__dirname + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.engine("html", require("ejs").renderFile);
+
+app.set("view engine", "html");
+
+app.set("views", __dirname + "/views");
+
+const connectMongo = (funct) =>
+	mongoClient.connect(url, function (error, client) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log("DB connected");
+			let db = client.db("ToDoList");
+			let tasks = db.collection("Tasks");
+			funct(tasks);
+		}
+	});
+
+app.post("/tasks/new", function (req, res) {
+	connectMongo((tasks) => {
+		try {
+			tasks.insertOne({
+				timestamp: new Date(),
+				description: req.body.description,
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	});
+	res.redirect("/");
+});
+
+app.get("/tasks/index", (req, res) => {
+	connectMongo((tasks) => {
+		try {
+			tasks.find({}).toArray((error, result) => {
+				res.send(JSON.stringify(result));
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	});
+});
+
+app.put("/tasks/update", function (req, res) {
+    console.log(req.body.info,req.body.id)
+	let o_id = new ObjectId(req.body.id)
+	connectMongo((tasks) => {
+		try {
+			tasks.updateOne(
+				{ _id: o_id },
+				{ $set: { description: req.body.description } }
+			);
+		} catch (err) {
+			console.log(err);
+		}
+	});
+});
+
+app.get('/tasks/update/:id',( req,res ) =>{
+	res.sendFile(__dirname + '/views/update.html');
+})
+
+app.delete("/tasks/delete/:id", (req, res) => {
+    let o_id = new ObjectId(req.params.id);
+    connectMongo((tasks) => {
+        tasks.findOneAndDelete({_id: o_id}, (err, result) => {
+            console.log(err, result);
+        })
+    })
+})
+
+app.get("/", function (req, res) {
+	res.render("index");
+});
+
+app.listen(8080);
---
+++ public/script.js
@@ -0,0 +1,31 @@
+let taskdelete = (tag) => {
+	fetch("/tasks/delete/" + tag.className, { method: "DELETE" });
+	window.location.href = "/";
+};
+
+let redirect = (tag) => {
+	console.log(tag.className);
+	window.location = "/tasks/update/" + tag.className;
+};
+
+function addNewTask(tag){
+    if(document.getElementById('new-task').style.display === 'block'){
+        document.getElementById('new-task').style.display = 'none';
+        tag.innerText = 'Add New Task';
+    }else{
+        document.getElementById('new-task').style.display = 'block';
+        tag.innerText = 'Close';
+    }
+}
+
+window.onload = async () => {
+	console.log("dom content loaded");
+	const res = await fetch("/tasks/index");
+	const data = await res.json();
+	data.forEach((item) => {
+		tasks.innerHTML += `
+        <li class='task-item'>${item.description}</li>
+        <button class=${item._id} onclick="redirect(this)">Update</button>
+        <button onclick='taskdelete(this)' class=${item._id}>Delete</button> <br/>`;
+	});
+};
